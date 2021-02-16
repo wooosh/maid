a7b48d971448016527b17b5540cb2bc9e034838b
wooosh <12696312+wooosh@users.noreply.github.com>
2021-02-15 21:09:16 -0500

    command stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module maid
 
+go 1.15
+
 require github.com/google/go-jsonnet v0.17.0
--- maid.go
+++ maid.go
@@ -7,13 +7,21 @@ import (
   "strings"
   "encoding/json"
   "io/ioutil"
+  "os/exec"
 
   "github.com/google/go-jsonnet"
 )
 
 type action struct {
+  Type string `json:"type"`
+
+  // type: write
   Path string `json:"path"`
   Contents string `json:"contents"`
+
+  // type: command
+  Command string `json:"command"`
+  Arguments []string `json:"arguments"`
 }
 
 func check(err error) {
@@ -53,12 +61,27 @@ func main() {
   check(err)
 
   for _, action := range actions {
-    path := action.Path
-    if strings.HasPrefix(path, "~/") {
-      path = filepath.Join(usr.HomeDir, path[2:])
+    switch action.Type {
+    case "write":
+      path := action.Path
+      if strings.HasPrefix(path, "~/") {
+        path = filepath.Join(usr.HomeDir, path[2:])
+      }
+      fmt.Println("writing:", path)
+      err := ioutil.WriteFile(path, []byte(action.Contents), 0644)
+      check(err)
+
+    case "command":
+      fmt.Println("command:", action.Command, strings.Join(action.Arguments, " "))
+      cmd := exec.Command(action.Command, action.Arguments...)
+
+      out, err := cmd.Output()
+      check(err)
+
+      fmt.Println(string(out))
+
+    default:
+      fmt.Println("invalid command type: " + action.Type)
     }
-    fmt.Println("writing ", path)
-    err := ioutil.WriteFile(path, []byte(action.Contents), 0644)
-    check(err)
   }
 }
